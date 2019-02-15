751367d83645deb22b80aee3d3aead4073c33a9f
汪韬 <452663934@qq.com>
2019-02-15 17:57:56 +0800

    :sparkle: 增加数量调整

--- src/views/donuts.vue
+++ src/views/donuts.vue
@@ -1,18 +1,24 @@
 <template>
-    <vego-canvas :width="canvasWidth" :height="canvasHeight">
-        <template v-for="don in donuts">
-            <donut
-                :geox="don.x"
-                :geoy="don.y"
-                :key="don.key">
-            </donut>
-        </template>
-    </vego-canvas>
+    <div>
+        <div class="form-group">
+            <label>DONUTS:</label>
+            <input v-model="num" placeholder="quantity of donuts" />
+        </div>
+        <div>FPS: {{fps}}</div>
+        <vego-canvas :width="canvasWidth" :height="canvasHeight">
+            <template v-for="don in donuts">
+                <donut
+                    :geox="don.x"
+                    :geoy="don.y"
+                    :key="don.key">
+                </donut>
+            </template>
+        </vego-canvas>
+    </div>
 </template>
 
 <script>
 import Donut from '../components/donut.vue';
-const num = 200;
 const nextFrame = window.requestAnimationFrame;
 export default {
     components: {
@@ -23,34 +29,66 @@ export default {
             canvasWidth: 960,
             canvasHeight: 380,
             donuts: [],
+            num: 200,
+            fps: undefined
         };
+    },
+    watch: {
+        num(val){
+            this.generateDonuts();
+        }
     },
     mounted() {
         const {
             canvasWidth,
             canvasHeight,
-            donuts,
+            num
         } = this;
         const radius = 50;
-        for (let i = 0; i < num; i++) {
-            donuts.push({
-                x: Math.random() * canvasWidth,
-                y: Math.random() * canvasHeight,
-                velX: Math.random() * 10 - 5,
-                velY: Math.random() * 10 - 5,
-            });
-        }
+        this.generateDonuts();
         const w = canvasWidth + radius * 2;
         const h = canvasHeight + radius * 2;
+        const times = [];
         const tick = () => {
-            for (let i = 0; i < num; i++) {
-                donuts[i].x = (donuts[i].x + radius + donuts[i].velX + w) % w - radius;
-                donuts[i].y = (donuts[i].y + radius + donuts[i].velY + h) % h - radius;
+
+            this.donuts.forEach(( d )=>{
+                d.x = (d.x + radius + d.velX + w) % w - radius;
+                d.y = (d.y + radius + d.velY + w) % w - radius;
+            });
+            const now = performance.now();
+            while (times.length > 0 && times[0] <= now - 1000) {
+                times.shift();
             }
+            times.push(now);
+            this.fps = times.length;
+
+            // for (let i = 0; i < this.num; i++) {
+            //     donuts[i].x = (donuts[i].x + radius + donuts[i].velX + w) % w - radius;
+            //     donuts[i].y = (donuts[i].y + radius + donuts[i].velY + h) % h - radius;
+            // }
             nextFrame(tick);
         };
         nextFrame(tick);
     },
+    methods: {
+        generateDonuts(){
+            const d = [];
+            const {
+                num,
+                canvasWidth,
+                canvasHeight
+            } = this;
+            for (let i = 0; i < num; i++) {
+                d.push({
+                    x: Math.random() * canvasWidth,
+                    y: Math.random() * canvasHeight,
+                    velX: Math.random() * 10 - 5,
+                    velY: Math.random() * 10 - 5,
+                });
+            }
+            this.donuts = d;
+        }
+    }
 };
 </script>
 

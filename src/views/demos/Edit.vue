121ae826928262fea7a39cd18ec1ef8e9606067e
汪韬 <452663934@qq.com>
2019-02-28 21:45:35 +0800

    :bug: 页面无法更新

--- src/views/demos/Edit.vue
+++ src/views/demos/Edit.vue
@@ -30,45 +30,7 @@
                 <div class="content text-left markdown-max-width" id="content4">
                     <parint-target-m-d></parint-target-m-d>
                 </div>
-                <!-- <div class="content text-left" id="content5">
-                    <utils-m-d></utils-m-d>
-                </div> -->
             </div>
-            <!-- <div class="collapsible">
-                <input id="collapsible3" type="checkbox" name="editMD">
-                <label for="collapsible3">Edit.vue</label>
-                <div class="collapsible-body text-left" style="overflow: scroll;">
-                    <edit-m-d></edit-m-d>
-                </div>
-            </div>
-            <div class="collapsible">
-                <input id="collapsible4" type="checkbox" name="paintBtnMD">
-                <label for="collapsible4">paint-btn.vue</label>
-                <div class="collapsible-body text-left" style="overflow: scroll;">
-                    <paint-btn-m-d></paint-btn-m-d>
-                </div>
-            </div>
-            <div class="collapsible">
-                <input id="collapsible5" type="checkbox" name="paintPanelMD">
-                <label for="collapsible5">paint-panel.vue</label>
-                <div class="collapsible-body text-left" style="overflow: scroll;">
-                    <paint-panel-m-d></paint-panel-m-d>
-                </div>
-            </div>
-            <div class="collapsible">
-                <input id="collapsible6" type="checkbox" name="parintTargetMD">
-                <label for="collapsible6">paint-target.vue</label>
-                <div class="collapsible-body text-left" style="overflow: scroll;">
-                    <parint-target-m-d></parint-target-m-d>
-                </div>
-            </div>
-            <div class="collapsible">
-                <input id="collapsible7" type="checkbox" name="utilsMD">
-                <label for="collapsible7">utils</label>
-                <div class="collapsible-body text-left" style="overflow: scroll;">
-                    <utils-m-d></utils-m-d>
-                </div>
-            </div> -->
         </div>
 </template>
 
